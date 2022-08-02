3a82cf3221a4451f2a0398f91d0dbdd0d8baf6f2
rockgustavo <grcuritiba@gmail.com>
2022-08-02 11:22:50 -0300

    02-08-2022

---
+++ rhPayroll/src/main/java/com/rockgustavo/RhPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.rockgustavo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhPayrollApplication.class, args);
+	}
+
+}
---
+++ rhPayroll/src/main/java/com/rockgustavo/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.rockgustavo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+	ModelAndView mv = new ModelAndView();
+
+	@GetMapping("/")
+	public ModelAndView index() {
+		mv.setViewName("home/index");
+		return mv;
+	}
+
+	@GetMapping("/logar")
+	public ModelAndView index2() {
+		mv.setViewName("home/index");
+		return mv;
+	}
+
+	@PostMapping("/logar")
+	public ModelAndView create(String name, String password) {
+		System.out.println("LOGIN: " + name + " - SENHA: " + password);
+
+		mv.setViewName("home/index");
+		return mv;
+	}
+
+}
