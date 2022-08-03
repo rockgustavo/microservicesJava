4ffafb50d92435c9f156c0eb0ed5e6aa52ab9291
rockgustavo <grcuritiba@gmail.com>
2022-08-03 09:59:58 -0300

    03-08-2022

---
+++ rhEurekaServer/src/main/java/com/rockgustavo/RhEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.rockgustavo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RhEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhEurekaServerApplication.class, args);
+	}
+
+}
