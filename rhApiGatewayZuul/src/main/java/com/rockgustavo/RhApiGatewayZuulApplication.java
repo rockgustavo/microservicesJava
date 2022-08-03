3ca8501dd90ae209e5bedb5fd3ca35f58f8640c7
rockgustavo <grcuritiba@gmail.com>
2022-08-03 11:30:38 -0300

    03-08-2022

---
+++ rhApiGatewayZuul/src/main/java/com/rockgustavo/RhApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.rockgustavo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhApiGatewayZuulApplication.class, args);
+	}
+
+}
