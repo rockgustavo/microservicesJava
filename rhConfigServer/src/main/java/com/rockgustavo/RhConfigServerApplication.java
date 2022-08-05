a9a7dac7f06bb75cc4d3b090afb76385e5e9b253
rockgustavo <grcuritiba@gmail.com>
2022-08-04 21:38:37 -0300

    Private Git Repository

--- rhConfigServer/src/main/java/com/rockgustavo/RhConfigServerApplication.java
+++ rhConfigServer/src/main/java/com/rockgustavo/RhConfigServerApplication.java
@@ -1,10 +1,12 @@
 package com.rockgustavo;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @EnableConfigServer
 @SpringBootApplication
