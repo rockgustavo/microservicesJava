3561aba052167b707990dbfb586b75967e900044
rockgustavo <grcuritiba@gmail.com>
2022-08-08 11:30:11 -0300

    Login, JWT token

--- rhOauth/src/main/java/com/rockgustavo/service/UserService.java
+++ rhOauth/src/main/java/com/rockgustavo/service/UserService.java
@@ -3,13 +3,16 @@ package com.rockgustavo.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.rockgustavo.feignclients.UserFeignClient;
 import com.rockgustavo.model.User;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	@Autowired
@@ -20,6 +23,17 @@ public class UserService {
 		if (user == null) {
 			logger.error("Email not found: " + email);
 			throw new IllegalArgumentException("Email not found");
+		}
+		logger.error("Email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + email);
+			throw new UsernameNotFoundException("Email not found");
 		}
 		logger.error("Email found: " + email);
 		return user;
